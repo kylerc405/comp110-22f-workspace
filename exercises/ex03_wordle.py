4b4b885bfa49e2ee098dd07b1c47a1d898223189
Kyler Chen <kylerc@unc.edu>
2022-09-14 20:06:20 -0400

    final ex03 2

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,4 +1,4 @@
-"""EX03 - Wordle"""
+"""EX03 - Wordle."""
 __author__ = "730564343"
 
 WHITE_BOX: str = "\U00002B1C"
@@ -7,27 +7,29 @@ YELLOW_BOX: str = "\U0001F7E8"
 
 
 def contains_char(word: str, guess_char: str) -> bool:
-    """contains_char searches for whether or not the secret word contains the character that has been guessed"""
-
+    """contains_char searches for whether or not the secret word contains the character that has been guessed."""
     assert len(guess_char) == 1
 
     i = 0
 
-    #Searches for any occurrences of the guessed letter
+    # Searches for any occurrences of the guessed letter
     while i < len(word):
         if guess_char == word[i]:
             return True
         else:
-            i+=1
+            i += 1
 
     return False
 
+
 def emojified(guess: str, secret: str) -> str:
+    """produces emoji output of green, yellow, or white boxes."""
     assert len(guess) == len(secret)
     emoji: str = ""
 
     i = 0
 
+    # Adds a green box if the letters match, a yellow box if the guessed letter is found elsewhere, and a white box if the letter is not found
     while i < len(secret):
         if guess[i] == secret[i]:
             emoji += GREEN_BOX
@@ -41,13 +43,16 @@ def emojified(guess: str, secret: str) -> str:
 
     return emoji
 
+
 def input_guess(length: int) -> str:
+    """obtains a guess from the player, and ensures the length matches the secret word."""
     word: str = input(f"enter a {length} character word: ")
 
     while len(word) != length:
         word = input(f"That wasn't {length} chars! Try again: ")
     
     return word
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
@@ -61,26 +66,26 @@ def main() -> None:
     correct: str = ""
     i = 0
 
+    # Creates a string of all correct guess emojis 
     while i < len(secret_word):
         correct += GREEN_BOX
         i += 1
-
-
+    # gives the user 6 turns to guess the correct word
     while won == False and turn <= 6:
         print(f"=== Turn {turn}/6 ===")
         guess = input_guess(len(secret_word))
         print(guess)
         emoji_string = emojified(guess, secret_word)
         print(emoji_string)
-
+        
         if emoji_string == correct:
             print(f"You won in {turn}/6 turns!")
             won = True
         elif emoji_string != correct:
             turn += 1
-    
     if turn == 7:
         print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
